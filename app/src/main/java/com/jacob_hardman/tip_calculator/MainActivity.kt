c21218795299f9163a4afaee93734e0051c3f5e5
Jacob Hardman <jacobhardmanbusiness@gmail.com>
2020-03-08 20:31:16 -0600

    add about menu functions

--- app/src/main/java/com/jacob_hardman/tip_calculator/MainActivity.kt
+++ app/src/main/java/com/jacob_hardman/tip_calculator/MainActivity.kt
@@ -2,11 +2,37 @@ package com.jacob_hardman.tip_calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AlertDialog
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    //About Menu Functions
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.about_item) {
+            showAboutInfo()
+        }
+        return true
+    }
+
+    private fun showAboutInfo() {
+        val aboutTitle = getString(R.string.about_title)
+        val aboutMessage = getString(R.string.about_message)
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle(aboutTitle)
+        builder.setMessage(aboutMessage)
+        builder.create().show()
     }
 }
